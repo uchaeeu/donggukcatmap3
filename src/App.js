0766ec1ca44e365e8c9703b3322710d26db31536
uchaeeu <chaeery@dgu.ac.kr>
2025-08-09 17:16:07 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FortuneGame from "./routes/FortuneGame";
-import './App.css';
 
-function App() {
+// 네 파일
+import FortuneGame from "./routes/FortuneGame";
+
+// 팀원 파일
+import MainScreen from "./routes/MainScreen";
+import UploadScreen from "./routes/UploadScreen";
+import SearchScreen from "./routes/SearchScreen";
+import TrendScreen from "./routes/TrendScreen";
+import LocationScreen from "./routes/LocationScreen";
+
+import "./App.css";
+
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FortuneGame />} />
-        {/* 여기에 다른 팀원들의 페이지들을 추가할 수 있습니다 */}
-        {/* 예: <Route path="/other-page" element={<OtherPage />} /> */}
+        {/* 홈은 팀원 메인으로 */}
+        <Route path="/" element={<MainScreen />} />
+
+        {/* 네 게임 페이지는 /game */}
+        <Route path="/game" element={<FortuneGame />} />
+
+        {/* 팀원 라우트들 */}
+        <Route path="/posts" element={<UploadScreen />} />
+        <Route path="/posts/:postId" element={<h1>Test Page</h1>} />
+        <Route path="/tags" element={<SearchScreen />} />
+        <Route path="/trend" element={<TrendScreen />} />
+        <Route path="/location" element={<LocationScreen />} />
+
+        {/* 404 */}
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+
